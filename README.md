28d16f782f56258808b6518c11964fd8da4095c3
pfhuamanflores <paulohfaqp@gmail.com>
2024-03-13 20:29:35 -0400

    last1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # My_project
 Proyecto final sprint 5
+Para el proyecto del presente sprint se utilizaron las herramientas aprendidas en él y así crear una aplicación sencilla utilizando una base de datos de anuncios de ventas de autos en la cual se muestra un botón y una casilla de verificación que al ser presionados grafican un histograma y un gráfico de dispersión respectivamente.
--- app.py
+++ app.py
@@ -7,25 +7,25 @@ st.header('Anuncios de ventas de coches')
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
         
-    if hist_button: # al hacer clic en el botón
+if hist_button: # al hacer clic en el botón
         # escribir un mensaje
-        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
         # crear un histograma
-        fig = px.histogram(car_data, x="odometer", color='condition') 
+    fig = px.histogram(car_data, x="odometer", color='condition') 
         
         # mostrar un gráfico Plotly interactivo
-        st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
     
     # crear una casilla de verificación
 build_scatter = st.checkbox('Construir el gráfico de disperción')
 
-    if build_scatter: # si la casilla de verificación está seleccionada
+if build_scatter: # si la casilla de verificación está seleccionada
         #Escribir un mensaje
-        st.write('Construir un grafico de disperción utilizando la columna odómetro y precio')
+    st.write('Construir un grafico de disperción utilizando la columna odómetro y precio')
         
         #Crear un gráfico de disperción
-        fig1 = px.scatter(car_data, x="odometer", y="price", color='model_year') # crear un gráfico de dispersión
+    fig1 = px.scatter(car_data, x="odometer", y="price", color='model_year') # crear un gráfico de dispersión
 
         # mostrar un gráfico Plotly interactivo
-        st.plotly_chart(fig1, use_container_width=True)
+    st.plotly_chart(fig1, use_container_width=True)
